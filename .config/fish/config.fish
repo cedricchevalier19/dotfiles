b5be5a87f8b5de707df8aca9c0fadae8db3fd4a8
Cedric Chevalier <cedric.chevalier019@proton.me>
2023-12-03 19:13:23 +0100

    Better fish config
    Set up only for interactive shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,20 @@
-fundle plugin 'edc/bass'
+if status is-interactive
+    # Commands to run in interactive sessions can go here
 
-fundle init
+    fundle plugin edc/bass
 
-starship init fish | source
+    fundle init
 
-# config.fish
-if not pgrep -f ssh-agent > /dev/null
-  eval (ssh-agent -c)
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    starship init fish | source
+
+    # config.fish
+    if not pgrep -f ssh-agent >/dev/null
+        eval (ssh-agent -c)
+        set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+        set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+        set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    end
+
+    atuin init --disable-up-arrow fish | source
+
 end
-
-atuin init --disable-up-arrow fish | source
-
